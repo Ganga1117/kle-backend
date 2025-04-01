cc4b773a529b1fb19ef58003e5b6ce8efc325d58
Ganga1117 <gangammak333@gmail.com>
2025-04-01 10:33:33 +0530

    project completed

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const {Cart} = require('./model/Cart');
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-mongoose.connect('mongodb://127.0.0.1:27017/kleecom')
+
+
+let MONGODB_URL = 'mongodb+srv://gangammak333:tnskqCNySaiXCOZS@cluster0.atqrbmh.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log('db connected');
 }).catch((err)=>{
